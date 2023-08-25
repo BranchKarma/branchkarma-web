77def111beb05753a293fa2b50f5688c285de90e
aslamPathan1069 <aslampathan@branchkarma.com>
2023-08-25 15:12:12 +0530

    added new component

---
+++ src/app/layout/square-cards/square-cards.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SquareCardsComponent } from './square-cards.component';
+
+describe('SquareCardsComponent', () => {
+  let component: SquareCardsComponent;
+  let fixture: ComponentFixture<SquareCardsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SquareCardsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SquareCardsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
