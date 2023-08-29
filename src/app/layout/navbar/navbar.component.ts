603813da1c261add546bee34b9b5f78c13e4ae0b
aslamPathan1069 <aslampathan@branchkarma.com>
2023-08-29 16:33:27 +0530

    added reponsive navbar

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-
+  showMenu = false;
+  toggleNavbar() {
+    this.showMenu = !this.showMenu;
+  }
 }
