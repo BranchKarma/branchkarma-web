62ef1820904c517d926a9e23e90e71eb75538207
Abhishek Vishwakarma <52538014+abhigdrv@users.noreply.github.com>
2024-02-23 20:32:08 +0530

    feat: Teams page added

--- src/app/pages/teams/teams.component.ts
+++ src/app/pages/teams/teams.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare var particlesJS: any;
 @Component({
   selector: 'app-teams',
   templateUrl: './teams.component.html',
-  styleUrls: ['./teams.component.scss']
+  styleUrls: ['./teams.component.scss'],
 })
-export class TeamsComponent {
-
+export class TeamsComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {
+    particlesJS.load('particles-js', 'assets/particles.json', null);
+  }
 }
